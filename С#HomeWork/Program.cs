a8ede2602d08748836d80f6ae6d8d8d49223abac
Olga <o.bolshakova1991@yandex.ru>
2022-08-23 06:52:46 +0300

    Домашняя работа

---
+++ С#HomeWork/Program.cs
@@ -0,0 +1,96 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.Write ("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + " больше, чем " + number2);
+}
+else
+{
+    Console.WriteLine(number1 + " меньше, чем " + number2);
+}
+
+
+*/
+
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    Console.WriteLine("Из введенных чисел максимальным является  " + number2);
+}
+else 
+{
+    if(number3 > max)
+    {
+        Console.WriteLine("Из введенных чисел максимальным является  " + number3);
+    }
+    else
+    {
+        Console.WriteLine("Из введенных чисел максимальным является  " + number1);
+
+    }
+}
+
+
+*/
+
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является ЧЕТНЫМ ");
+}
+else
+{
+    Console.WriteLine("Число " + number + " является НЕЧЕТНЫМ");
+
+}
+
+*/
+
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+/*Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while (current <= number)
+{
+    if(current % 2 == 0)
+    {
+        Console.Write(current + " ");
+
+    }
+    current ++;    
+}
+*/
